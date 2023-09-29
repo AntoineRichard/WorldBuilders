d1a2b72bc4b041edd0eaf77338d81b5453150a73
AntoineRichard <antoine.richard@uni.lu>
2023-09-29 13:57:55 +0200

    Added doc and fixed the XformOps issue...

--- pxr_utils.py
+++ pxr_utils.py
@@ -1,32 +1,86 @@
+from pxr import UsdGeom, Gf, Sdf, UsdPhysics, UsdShade, Usd, Vt
+from typing import Tuple, Union, List
+from omni.physx.scripts import utils
+import numpy as np
 import omni
 import os
-import numpy as np
-from pxr import UsdGeom, Gf, Sdf, UsdPhysics, UsdShade, Usd, Vt
-from omni.physx.scripts import utils
 
+def loadStage(path: str,
+              ) -> None:
+    """
+    Loads the stage from the given path.
+    
+    Args:
+        path (str): The path to the stage."""
 
-#from semantics.schema.editor import PrimSemanticData
-
-def loadStage(path: str):
     omni.usd.get_context().open_stage(path)
 
-def saveStage(path: str):
+def saveStage(path: str,
+              ) -> None:
+    """
+    Saves the stage to the given path.
+    
+    Args:
+        path (str): The path to the stage."""
+    
     omni.usd.get_context().save_as_stage(path, None)
 
-def newStage():
+def newStage() -> None:
+    """
+    Creates a new stage.
+    
+    Args:
+        path (str): The path to the stage."""
+    
     omni.usd.get_context().new_stage()
 
-def closeStage():
+def closeStage() -> None:
+    """
+    Closes the current stage."""
+
     omni.usd.get_context().close_stage()
 
-def setDefaultPrim(stage, path):
+def setDefaultPrim(stage: Usd.Stage,
+                   path:str,
+                   ) -> None:
+    """
+    Sets the default prim of the stage.
+    
+    Args:
+        stage (Usd.Stage): The stage.
+        path (str): The path to the default prim."""
+    
     prim = stage.GetPrimAtPath(path)
     stage.SetDefaultPrim(prim)
 
-def movePrim(path_from, path_to):
+def movePrim(path_from:str,
+             path_to:str,
+             ) -> None:
+    """
+    Moves a prim from one location to another.
+    There has to be some native PXR function for this, but I couldn't find it.
+    
+    Args:
+        path_from (str): The path to the prim to move.
+        path_to (str): The path to the new location of the prim."""
+
     omni.kit.commands.execute('MovePrim',path_from=path_from, path_to=path_to)
 
-def createXform(stage, path, add_default_op=False):
+def createXform(stage: Usd.Stage,
+                path:str,
+                add_default_op:bool=False,
+                ) -> Tuple[Usd.Prim, str]:
+    """
+    Creates a Xform prim and adds it to the stage.
+    
+    Args:
+        stage (Usd.Stage): The stage.
+        path (str): The path to the prim.
+        add_default_op (bool, optional): Whether to add the default ops to the prim. Defaults to False.
+        
+    Returns:
+        Tuple[Usd.Prim, str]: The prim and its path."""
+    
     prim_path = omni.usd.get_stage_next_free_path(stage, path, False)
     obj_prim = stage.DefinePrim(prim_path, "Xform")
 
@@ -34,16 +88,39 @@ def createXform(stage, path, add_default_op=False):
         addDefaultOps(obj_prim)
     return obj_prim, prim_path
 
-def addDefaultOps(prim):
-    prim = UsdGeom.Xform(prim)
-    prim.ClearXformOpOrder()
+def addDefaultOps(prim: Usd.Prim,
+                  ) -> UsdGeom.Xform:
+    """
+    Adds the default ops to a prim.
+    
+    Args:
+        prim (Usd.Prim): The prim.
+    
+    Returns:
+        UsdGeom.Xform: The Xform of the prim."""
+    
+    xform = UsdGeom.Xform(prim)
+    xform.ClearXformOpOrder()
 
-    prim.AddTranslateOp(precision = UsdGeom.XformOp.PrecisionDouble)
-    prim.AddOrientOp(precision = UsdGeom.XformOp.PrecisionDouble)
-    prim.AddScaleOp(precision = UsdGeom.XformOp.PrecisionDouble)
-    return prim
+    xform.AddTranslateOp(precision = UsdGeom.XformOp.PrecisionDouble)
+    xform.AddOrientOp(precision = UsdGeom.XformOp.PrecisionDouble)
+    xform.AddScaleOp(precision = UsdGeom.XformOp.PrecisionDouble)
+    return xform
 
-def setDefaultOps(xform, pos: tuple, rot: tuple, scale: tuple):
+def setDefaultOps(xform: UsdGeom.Xform,
+                  pos: Tuple[float, float, float],
+                  rot: Tuple[float, float, float, float],
+                  scale: Tuple[float, float, float],
+                  ) -> None:
+    """
+    Sets the default ops of a Xform prim using lists instead of the native values (Gf.Vec3d, Gf.Vec3f, Gf.Quatd, Gf.Quatf).
+    
+    Args:
+        xform (UsdGeom.Xform): The Xform prim.
+        pos (tuple): The position.
+        rot (tuple): The rotation as a quaternion.
+        scale (tuple): The scale."""
+    
     xform_ops = xform.GetOrderedXformOps()
     xform_ops[0].Set(Gf.Vec3d(float(pos[0]), float(pos[1]), float(pos[2])))
     try:
@@ -52,19 +129,39 @@ def setDefaultOps(xform, pos: tuple, rot: tuple, scale: tuple):
         xform_ops[1].Set(Gf.Quatd(float(rot[3]), float(rot[0]), float(rot[1]), float(rot[2])))
     xform_ops[2].Set(Gf.Vec3d(float(scale[0]), float(scale[1]), float(scale[2])))
 
-def setDefaultOpsTyped(xform, pos, rot, scale):
+def setDefaultOpsTyped(xform: UsdGeom.Xform,
+                       pos: Union[Gf.Vec3d, Gf.Vec3f],
+                       rot: Union[Gf.Quatd, Gf.Quatf],
+                       scale: Union[Gf.Vec3d, Gf.Vec3f],
+                       ) -> None:
+    """
+    Sets the default ops of a Xform prim using the native values (Gf.Vec3d, Gf.Vec3f, Gf.Quatd, Gf.Quatf).
+    
+    Args:
+        xform (UsdGeom.Xform): The Xform prim.
+        pos (Union[Gf.Vec3d, Gf.Vec3f]): The position.
+        rot (Union[Gf.Quatd, Gf.Quatf]): The rotation as a quaternion.
+        scale (Union[Gf.Vec3d, Gf.Vec3f]): The scale."""
+
     xform_ops = xform.GetOrderedXformOps()
     xform_ops[0].Set(pos)
     xform_ops[1].Set(rot)
     xform_ops[2].Set(scale)
 
-def loadTexture(stage, mdl_path, scene_path, sub_intentifier):
-    #omni.kit.commands.execute(
-    #    "CreateAndBindMdlMaterialFromLibrary",
-    #    mdl_name=mdl_path,
-    #    mtl_name=mdl_name,
-    #    mtl_created_list=[os.path.join(scene_path,mdl_name)],
-    #)
+def loadTexture(stage: Usd.Stage,
+                mdl_path: str,
+                scene_path: str,
+                sub_intentifier: str,
+                ) -> None:
+    """
+    Loads a texture from a MDL file and adds it to the stage.
+    
+    Args:
+        stage (Usd.Stage): The stage.
+        mdl_path (str): The path to the MDL file.
+        scene_path (str): The path to the scene.
+        sub_intentifier (str): The sub identifier of the texture."""
+    
     mtl_path = Sdf.Path(scene_path)
     mtl = UsdShade.Material.Define(stage, mtl_path)
     shader = UsdShade.Shader.Define(stage, mtl_path.AppendPath("Shader"))
@@ -80,39 +177,76 @@ def loadTexture(stage, mdl_path, scene_path, sub_intentifier):
     material = UsdShade.Material(mtl_prim)
     return material
 
-def applyMaterial(prim, material):
+def applyMaterial(prim: Usd.Prim,
+                  material: UsdShade.Material,
+                  ) -> None:
+    """
+    Applies a material to a prim.
+    
+    Args:
+        prim (Usd.Prim): The prim.
+        material (UsdShade.Material): The material."""
+
     UsdShade.MaterialBindingAPI(prim).Bind(material, UsdShade.Tokens.strongerThanDescendants)
 
-def applyMaterialFromPath(stage, prim_path, material_path):
+def applyMaterialFromPath(stage: Usd.Stage,
+                          prim_path: str,
+                          material_path: str,
+                          ) -> None:
+    """
+    Applies a material to a prim.
+    
+    Args:
+        stage (Usd.Stage): The stage.
+        prim_path (str): The path to the prim.
+        material_path (str): The path to the material."""
+    
     mtl_prim = stage.GetPrimAtPath(material_path)
     prim = stage.GetPrimAtPath(prim_path)
     material = UsdShade.Material(mtl_prim)
     UsdShade.MaterialBindingAPI(prim).Bind(material, UsdShade.Tokens.strongerThanDescendants)
 
-def createObject(prefix,
-    stage,
-    path,
-    position=Gf.Vec3d(0, 0, 0),
-    rotation=Gf.Quatd(0,0,0,1),
-    scale=Gf.Vec3d(1,1,1),
-    is_instance=True,
-) -> tuple:
+def createObject(prefix: str,
+    stage: Usd.Stage,
+    path: str,
+    position: Gf.Vec3d = Gf.Vec3d(0, 0, 0),
+    rotation: Gf.Quatd = Gf.Quatd(0,0,0,1),
+    scale: Gf.Vec3d = Gf.Vec3d(1,1,1),
+    is_instance: bool = True,
+    ) -> Tuple[Usd.Prim, str]:
     """
     Creates a 3D object from a USD file and adds it to the stage.
-    """
+    
+    Args:
+        prefix (str): The prefix of the object.
+        stage (Usd.Stage): The stage.
+        path (str): The path to the USD file.
+        position (Gf.Vec3d, optional): The position of the object. Defaults to Gf.Vec3d(0, 0, 0).
+        rotation (Gf.Quatd, optional): The rotation of the object. Defaults to Gf.Quatd(0,0,0,1).
+        scale (Gf.Vec3d, optional): The scale of the object. Defaults to Gf.Vec3d(1,1,1).
+        is_instance (bool, optional): Whether the object is an instance or not. Defaults to True."""
+    
     obj_prim, prim_path = createXform(stage, prefix)
+    xform = UsdGeom.Xformable(obj_prim)
+    addDefaultOps(xform)
+    setDefaultOpsTyped(xform, position, rotation, scale)
     obj_prim.GetReferences().AddReference(path)
     if is_instance:
         obj_prim.SetInstanceable(True)
-    xform = UsdGeom.Xformable(obj_prim)
-    #setScale(xform, scale)
-    addDefaultOps(xform)
-    setDefaultOpsTyped(xform, position, rotation, scale)
-    #print(rotation, position)
-    #setTransform(xform, getTransform(rotation, position))
     return obj_prim, prim_path
 
-def addCollision(stage, path, mode="none"):
+def addCollision(stage: Usd.Stage,
+                 path: str,
+                 mode: str = "none",
+                 ) -> None:
+    """
+    Adds a collision to a prim.
+    
+    Args:
+        stage (Usd.Stage): The stage.
+        path (str): The path to the prim.
+        mode (str, optional): The mode of the collision. Defaults to "none"."""
+
     # Checks that the mode selected by the user is correct.
     accepted_modes = ["none", "convexHull", "convexDecomposition", "meshSimplification", "boundingSphere", "boundingCube"]
     assert mode in accepted_modes, "Decimation mode: "+mode+" for colliders unknown."
@@ -120,21 +254,63 @@ def addCollision(stage, path, mode="none"):
     prim = stage.GetPrimAtPath(path)
     utils.setCollider(prim, approximationShape=mode)
 
-def removeCollision(stage, path):
+def removeCollision(stage: Usd.Stage,
+                    path: str,
+                    ) -> None:
+    """
+    Removes a collision from a prim.
+    
+    Args:
+        stage (Usd.Stage): The stage.
+        path (str): The path to the prim."""
+    
     # Get the prim and remove collisions.
     prim = stage.GetPrimAtPath(path)
     utils.removeCollider(prim)
 
-def deletePrim(stage, path):
+def deletePrim(stage: Usd.Stage,
+               path: str,
+               ) -> None:
+    """
+    Deletes a prim from the stage.
+    
+    Args:
+        stage (Usd.Stage): The stage.
+        path (str): The path to the prim."""
+    
     # Deletes a prim from the stage.
     stage.RemovePrim(path)
 
-def createStandaloneInstance(stage, path):
+def createStandaloneInstance(stage: Usd.Stage,
+                             path: str,
+                             ) -> UsdGeom.PointInstancer:
+    """
+    Creates only the instance object.
+    
+    Args:
+        stage (Usd.Stage): The stage.
+        path (str): The path to the instance.
+    
+    Returns:
+        UsdGeom.PointInstancer: The instance."""
+    
     # Creates and instancer.
+    createXform(stage, path, add_default_op=True)
     instancer = UsdGeom.PointInstancer.Define(stage, path)
     return instancer
 
-def createInstancerAndCache(stage, path, asset_list):
+def createInstancerAndCache(stage: Usd.Stage,
+                            path: str,
+                            asset_list: List[str],
+                            ) -> None:
+    """
+    Creates an instancer and its cache.
+    
+    Args:
+        stage (Usd.Stage): The stage.
+        path (str): The path to the instancer.
+        asset_list (List[str]): The list of assets to cache."""
+    
     # Creates a point instancer
     instancer = createStandaloneInstance(stage, path)
     # Creates a Xform to cache the assets to.
@@ -152,7 +328,18 @@ def createInstancerAndCache(stage, path, asset_list):
     # Set some dummy parameters
     setInstancerParameters(stage, path, pos=np.zeros((1,3))) 
 
-def createInstancerFromCache(stage, path, cache_path):
+def createInstancerFromCache(stage: Usd.Stage,
+                             path: str,
+                             cache_path: str,
+                             ) -> None:
+    """
+    Creates an instancer from a cache.
+    
+    Args:
+        stage (Usd.Stage): The stage.
+        path (str): The path to the instancer.
+        cache_path (str): The path to the cache."""
+    
     # Creates a point instancer
     instancer = createStandaloneInstance(stage, path)
     # Add each asset to the scene in the cache.
@@ -161,7 +348,24 @@ def createInstancerFromCache(stage, path, cache_path):
     # Set some dummy parameters
     setInstancerParameters(stage, path, pos=np.zeros((1,3))) 
 
-def setInstancerParameters(stage, path, pos: np.ndarray([],dtype=np.float), ids: np.ndarray([],dtype=int) = None, scale: np.ndarray([],dtype=np.float) = None, quat: np.ndarray([],dtype=np.float) = None):
+def setInstancerParameters(stage: Usd.Stage,
+                           path: str,
+                           pos: np.ndarray([],dtype=np.float),
+                           ids: np.ndarray([],dtype=int) = None,
+                           scale: np.ndarray([],dtype=np.float) = None,
+                           quat: np.ndarray([],dtype=np.float) = None,
+                           ) -> None:
+    """
+    Sets the parameters of an instancer.
+    
+    Args:
+        stage (Usd.Stage): The stage.
+        path (str): The path to the instancer.
+        pos (np.ndarray): The positions of the instances.
+        ids (np.ndarray, optional): The ids of the instances. Defaults to None.
+        scale (np.ndarray, optional): The scale of the instances. Defaults to None.
+        quat (np.ndarray, optional): The orientation of the instances. Defaults to None."""
+
     num = pos.shape[0]
     instancer_prim = stage.GetPrimAtPath(path)
     num_prototypes = len(instancer_prim.GetRelationship("prototypes").GetTargets())
@@ -183,7 +387,16 @@ def setInstancerParameters(stage, path, pos: np.ndarray([],dtype=np.float), ids:
     instancer_prim.GetAttribute("protoIndices").Set(ids)
     updateExtent(stage, path)
     
-def updateExtent(stage, instancer_path):
+def updateExtent(stage: Usd.Stage,
+                 instancer_path: str,
+                 ) -> None:
+    """
+    Updates the extent of an instancer.
+    
+    Args:
+        stage (Usd.Stage): The stage.
+        instancer_path (str): The path to the instancer."""
+    
     # Get the point instancer.
     instancer = UsdGeom.PointInstancer.Get(stage, instancer_path)
     # Compute the extent of the objetcs.
@@ -194,7 +407,16 @@ def updateExtent(stage, instancer_path):
         Gf.Vec3f(extent[1]),
     ]))
 
-def enableSmoothShade(prim, extra_smooth=False):
+def enableSmoothShade(prim: Usd.Prim,
+                      extra_smooth: bool = False,
+                      ) -> None:
+    """
+    Enables smooth shading on a prim.
+    
+    Args:
+        prim (Usd.Prim): The prim on which to enable smooth shading.
+        extra_smooth (bool, optional): Toggles the use of the smooth method instead of catmullClark. Defaults to False."""
+
     # Sets the subdivision scheme to smooth.
     prim.GetAttribute("subdivisionScheme").Set(UsdGeom.Tokens.catmullClark)
     # Sets the triangle subdivision rule.
@@ -202,55 +424,3 @@ def enableSmoothShade(prim, extra_smooth=False):
         prim.GetAttribute("triangleSubdivisionRule").Set(UsdGeom.Tokens.smooth)
     else:
         prim.GetAttribute("triangleSubdivisionRule").Set(UsdGeom.Tokens.catmullClark)
-
-def getTransform(
-    rotation: Gf.Rotation,
-    position: Gf.Vec3d,
-) -> Gf.Matrix4d:
-    matrix_4d = Gf.Matrix4d().SetTranslate(position)
-    matrix_4d.SetRotateOnly(rotation)
-    print(matrix_4d)
-    return matrix_4d
-
-def setProperty(
-    xform: UsdGeom.Xformable,
-    value,
-    property,
-) -> None:
-    op = None
-    for xformOp in xform.GetOrderedXformOps():
-        if xformOp.GetOpType() == property:
-            op = xformOp
-    if op:
-        xform_op = op
-    else:
-        xform_op = xform.AddXformOp(
-            property,
-            UsdGeom.XformOp.PrecisionDouble,
-            "",
-        )
-    xform_op.Set(value)
-
-def setScale(
-    xform: UsdGeom.Xformable,
-    value,
-) -> None:
-    setProperty(xform, value, UsdGeom.XformOp.TypeScale)
-
-def setTranslate(
-    xform: UsdGeom.Xformable,
-    value,
-) -> None:
-    setProperty(xform, value, UsdGeom.XformOp.TypeTranslate)
-
-def setRotateXYZ(
-    xform: UsdGeom.Xformable,
-    value,
-) -> None:
-    setProperty(xform, value, UsdGeom.XformOp.TypeRotateXYZ)
-
-def setTransform(
-    xform: UsdGeom.Xformable,
-    value: Gf.Matrix4d,
-) -> None:
-    setProperty(xform, value, UsdGeom.XformOp.TypeTransform)
